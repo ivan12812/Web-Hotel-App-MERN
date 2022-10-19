394f5df1633cb7511ac4f833668928c54026f3ca
Ananda Bayu <anandabayu090@gmail.com>
2022-10-19 15:53:52 +0700

    authentication

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,93 @@
+import { useState } from "react";
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	Outlet,
+	Navigate,
+} from "react-router-dom";
+import NavBar from "./components/Nav-Bar";
+import LoginPage from "./pages/authentication/Login-Page";
+
+const Protected = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(
+		localStorage.getItem("TOKEN")
+	);
+
+	return isAuthenticated ? (
+		<>
+			<NavBar />
+			<Outlet />
+		</>
+	) : (
+		<Navigate to="/login" />
+	);
+};
+
+const AccessLoginPageHandler = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(
+		localStorage.getItem("TOKEN")
+	);
+
+	return isAuthenticated ? <Navigate to="/" /> : <LoginPage />;
+};
+
 function App() {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Protected />}>
+					{/* <Route path="transaction">
+						<Route path="borrows">
+							<Route
+								index
+								element={<BorrowsListPage />}
+							/>
+							<Route
+								path="create"
+								element={<CreateBorrowPage />}
+							/>
+						</Route>
+					</Route>
+					<Route path="master">
+						<Route path="cars">
+							<Route
+								index
+								element={<CarsListPage />}
+							/>
+							<Route
+								path="create"
+								element={<CreateCarPage />}
+							/>
+							<Route
+								path="update/:id"
+								element={<UpdateCarPage />}
+							/>
+						</Route>
+						<Route path="users">
+							<Route
+								index
+								element={<UserListPage />}
+							/>
+							<Route
+								path="create"
+								element={<CreateUserPage />}
+							/>
+							<Route
+								path="update/:id"
+								element={<UpdateUserPage />}
+							/>
+						</Route>
+					</Route> */}
+				</Route>
+				<Route path="/login" element={<AccessLoginPageHandler />} />
+				{/* <Route
+					path="*"
+					element={<Navigate to="/transaction/borrows" />}
+				/> */}
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/frontend/src/api/Helper.js
+++ src/frontend/src/api/Helper.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "http://localhost:3001/api";
 
 export { BASE_URL };
