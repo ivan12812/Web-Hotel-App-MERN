efcabed8f65dbf8e951ff71d22af0ab1e3302130
Ananda Bayu <anandabayu090@gmail.com>
2022-10-19 14:01:48 +0700

    finishing checkin & checkout

--- src/backend/controllers/index.js
+++ src/backend/controllers/index.js
@@ -2,11 +2,13 @@ const authController = require("./auth.controller");
 const { isAuthenticated, isAdmin } = require("../middlewares/auth.middleware");
 const userController = require("./user.controller");
 const roomController = require("./room.controller");
-// const borrowController = require("./borrow.controller");
+const checkinController = require("./checkin.controller");
+const checkoutController = require("./checkout.controller");
 
 module.exports = (app) => {
 	app.use("/api/auth", authController);
 	app.use("/api/user", [isAuthenticated, isAdmin], userController);
 	app.use("/api/room", [isAuthenticated, isAdmin], roomController);
-	// app.use("/borrow", [isAuthenticated], borrowController);
+	app.use("/api/checkin", [isAuthenticated], checkinController);
+	app.use("/api/checkout", [isAuthenticated], checkoutController)
 };
